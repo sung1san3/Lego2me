4b64a077556c3ef7964b472e2338d2fe7833c5be
min <hororonoodle@gmail.com>
2022-01-05 21:32:47 +0900

    feat: 프론트엔드 컨테이너로 만들어서 도커 컴포즈 추가

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+
+    return config;
+  },
+};
