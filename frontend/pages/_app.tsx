a6eda35d6cbb29864f3a3cad88dfa121b5095aaf
min <hororonoodle@gmail.com>
2022-01-07 13:53:15 +0900

    feat : add favicon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,15 +5,23 @@ import NextNProgress from "nextjs-progressbar";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
 
+        <title>Lego2me</title>
+      </head>
       <NextNProgress color="#D01020" />
       <Component {...pageProps} />
     </>
