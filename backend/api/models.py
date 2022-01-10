a80cafe674bf0899f834ceab4d0b5f5a398d8625
choiseyeon <choi20014830@gmail.com>
2022-01-10 15:16:52 +0900

    fix: django + mongo + docker

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+#from djongo import models
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class test(models.Model):
-    title = models.CharField(max_length=20)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=100, default='')
+    image_uri = models.CharField(max_length=100, default='')
+
+class img_upload(models.Model):
+    name = models.ImageField(upload_to="api", default='')
---
+++ backend/api/serializers.py
@@ -0,0 +1,4 @@
+# from rest_framework import serializers
+# from rest_framework_jwt.settings import api_settings
+# from django.contrib.auth.models import User
+
