77a0d643622a82086006819682a8180f0fd2996b
choiseyeon <choi20014830@gmail.com>
2022-01-12 16:05:14 +0900

    feat : GCS, mongoDB datasave

--- backend/api/img_upload.py
+++ backend/api/img_upload.py
@@ -3,9 +3,17 @@ import os, os.path
 
 from google.cloud import storage
 import glob
+from .serializers import Img_data_serializers
+from .models import Image_data
 
 # --------------[START storage_upload_file]
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
+def upload_blob(filename):
+    bucket_name = "lego_example"
+    source_file_name = "img/"+filename[0]
+    destination_blob_name = "temp/"+filename[0]
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="lego2me-image-d0a54bb93c41.json"
+
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
@@ -13,3 +21,10 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob.upload_from_filename(source_file_name)
 
     print("파일 {} 이 /{} 저장소에 업로드 되었습니다.".format(source_file_name, destination_blob_name))
+
+def db_save(filename, imguri):
+    img_data = Image_data()
+
+    img_data.name = filename
+    img_data.image_uri = imguri
+    img_data.save()
