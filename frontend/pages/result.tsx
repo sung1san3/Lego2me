2ba9175c1c5ba727bd6aee334a9dc6cd43fb10f8
min <hororonoodle@gmail.com>
2022-01-18 17:44:39 +0900

    styled : LegoItems into Array

--- frontend/pages/result.tsx
+++ frontend/pages/result.tsx
@@ -31,38 +31,86 @@ const Result: NextPage = () => {
   ];
   const topStyle = [
     {
-      img: "/items/top/top1_orange.png",
-      title: "top1",
+      img: "/legoItem/Top/Black_Shirts.png",
+      title: "Black_Shirts",
     },
     {
-      img: "/items/top/top2_green.png",
-      title: "top2",
+      img: "/legoItem/Top/Blue_Shirts.png",
+      title: "Blue_Shirts",
     },
     {
-      img: "/items/top/top3_blue.png",
-      title: "top3",
+      img: "/legoItem/Top/Brown_Shirts.png",
+      title: "Brown_Shirts",
     },
     {
-      img: "/items/top/red_shirts.png",
-      title: "top4",
+      img: "/legoItem/Top/Green_Shirts.png",
+      title: "Green_Shirts",
+    },
+    {
+      img: "/legoItem/Top/Grey_Shirts.png",
+      title: "Grey_Shirts",
+    },
+    {
+      img: "/legoItem/Top/Orange_Shirts.png",
+      title: "Orange_Shirts",
+    },
+    {
+      img: "/legoItem/Top/Purple_Shirts.png",
+      title: "Purple_Shirts",
+    },
+    {
+      img: "/legoItem/Top/Red_Shirts.png",
+      title: "Red_Shirts",
+    },
+    {
+      img: "/legoItem/Top/White_Shirts.png",
+      title: "White_Shirts",
+    },
+    {
+      img: "/legoItem/Top/Yellow_Shirts.png",
+      title: "Yellow_Shirts",
     },
   ];
   const bottomStyle = [
     {
-      img: "/items/bottom/btm1_blue.png",
-      title: "bottom1",
+      img: "/legoItem/Bottom/Black_Pants.png",
+      title: "Black_Pants",
     },
     {
-      img: "/items/bottom/btm2_red.png",
-      title: "bottom2",
+      img: "/legoItem/Bottom/Blue_Pants.png",
+      title: "Blue_Pants",
     },
     {
-      img: "/items/bottom/btm3_orange.png",
-      title: "bottom3",
+      img: "/legoItem/Bottom/Brown_Pants.png",
+      title: "Brown_Pants",
     },
     {
-      img: "/items/bottom/black_pants.png",
-      title: "bottom4",
+      img: "/legoItem/Bottom/Green_Pants.png",
+      title: "Green_Pants",
+    },
+    {
+      img: "/legoItem/Bottom/Grey_Pants.png",
+      title: "Grey_Pants",
+    },
+    {
+      img: "/legoItem/Bottom/Orange_Pants.png",
+      title: "Orange_Pants",
+    },
+    {
+      img: "/legoItem/Bottom/Purple_Pants.png",
+      title: "Purple_Pants",
+    },
+    {
+      img: "/legoItem/Bottom/Red_Pants.png",
+      title: "Red_Pants",
+    },
+    {
+      img: "/legoItem/Bottom/White_Pants.png",
+      title: "White_Pants",
+    },
+    {
+      img: "/legoItem/Bottom/Yellow_Pants.png",
+      title: "Yellow_Pants",
     },
   ];
 
